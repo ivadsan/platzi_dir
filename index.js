c080954b4c07ed44b094c3a9bef84fb5b9555421
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2020-05-01 04:11:30 -0500

    [Update] GraphQL course - Class 18

--- index.js
+++ index.js
@@ -1,39 +1,32 @@
-'use strict';
-const { graphql, buildSchema } = require('graphql');
-const express = require('express');
-const gqlMiddleware = require('express-graphql');
+'use strict'
+require('dotenv').config()
+const { makeExecutableSchema } = require('graphql-tools')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers.js')
 
-const app = express();
-const port = process.env.port || 3000;
+const app = express()
+const port = process.env.port || 3000
 
 // definiendo el esquema
-const schema = buildSchema(`
-  type Query {
-    hello: String,
-    saludo: String
-  }
-`);
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
+)
 
-//Configurar los resolvers
-
-const resolvers = {
-  hello: () => {
-    return 'hola Mundo';
-  },
-  saludo: () => {
-    return 'nuevo resolver';
-  },
-};
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 app.use(
   '/api',
   gqlMiddleware({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true,
+    graphiql: true
   })
-);
+)
 
 app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}/api`);
-});
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
